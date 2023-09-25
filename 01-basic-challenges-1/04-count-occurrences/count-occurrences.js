37fbfb13abdd2ea8bf6078c4319dc8467ff68e45
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-09-25 11:26:20 -0400

    Add 'toLowerCase' method to make 'str' case-insensitive.

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,5 +1,10 @@
 function countOccurrences(str, char) {
-	return str.split('').filter((letter) => letter === char).length;
+	return str
+		.toLowerCase()
+		.split('')
+		.filter((letter) => letter === char).length;
 }
+
+console.log(countOccurrences('chOoOOookie', 'o'));
 
 module.exports = countOccurrences;
