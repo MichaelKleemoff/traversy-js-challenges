342f78354ab78a4adf2ee08e8a6be7ac0a398533
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-11-20 10:21:53 -0500

    Add 2nd solution to 'titleCase'.

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,8 +1,20 @@
 function titleCase(str) {
-	return str
-		.split(' ')
-		.map((word) => `${word[0].toUpperCase()}${word.slice(1).toLowerCase()}`)
-		.join(' ');
+	// My solution 1 -
+	// return str
+	// 	.split(' ')
+	// 	.map((word) => `${word[0].toUpperCase()}${word.slice(1).toLowerCase()}`)
+	// 	.join(' ');
+
+	// My solution 2 -
+	let strArr = str.split(' ');
+	const firstCap = [];
+
+	for (let i = 0; i < strArr.length; i++) {
+		firstCap.push(
+			`${strArr[i][0].toUpperCase()}${strArr[i].slice(1).toLowerCase()}`
+		);
+	}
+	return firstCap.join(' ');
 }
 
 module.exports = titleCase;
