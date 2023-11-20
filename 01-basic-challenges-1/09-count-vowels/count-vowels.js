d8646a4938193fffbf72cdfd143dc3162f303ca6
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-11-20 15:47:56 -0500

    Update 'countVowels' with 'str[i].toLowerCase()' in for loop.

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -4,7 +4,7 @@ function countVowels(str) {
 	// const vowels = ['a', 'e', 'i', 'o', 'u'];
 
 	// for (let i = 0; i < str.length; i++) {
-	// 	if (vowels.includes(str[i])) {
+	// 	if (vowels.includes(str[i].toLowerCase())) {
 	// 		count++;
 	// 	}
 	// }
