529b362cbfb9ef5b425799a70e67197ea4b61147
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-11-20 16:58:16 -0500

    Finish 'fizzBuzzArray'.

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,19 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+	const fizzBuzz = [];
+
+	for (let i = 1; i <= num; i++) {
+		if (i % 3 === 0 && i % 5 === 0) {
+			fizzBuzz.push('FizzBuzz');
+		} else if (i % 3 === 0) {
+			fizzBuzz.push('Fizz');
+		} else if (i % 5 === 0) {
+			fizzBuzz.push('Buzz');
+		} else {
+			fizzBuzz.push(i);
+		}
+	}
+
+	return fizzBuzz;
+}
 
 module.exports = fizzBuzzArray;
