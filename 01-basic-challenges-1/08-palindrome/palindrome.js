7a3948fb0c9f0156bb6771bcd8897f3f195d3135
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-11-20 13:22:34 -0500

    Finish 'isPalindrome'.

--- 01-basic-challenges-1/08-palindrome/palindrome.js
+++ 01-basic-challenges-1/08-palindrome/palindrome.js
@@ -1,3 +1,12 @@
-function isPalindrome() {}
+function isPalindrome(str) {
+	let revStr = '';
+	const remSpacPunc = (str) => str.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+	for (let i = str.length - 1; i >= 0; i--) {
+		revStr += str[i];
+	}
+
+	return remSpacPunc(str) === remSpacPunc(revStr);
+}
 
 module.exports = isPalindrome;
