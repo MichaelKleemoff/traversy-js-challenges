75f86a5ab16ddd04ebe86e7699d23c77eb052b28
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-11-20 15:43:10 -0500

    Finish 'countVowels' with 2 solutions.

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,17 @@
-function countVowels() {}
+function countVowels(str) {
+	// Solution 1 -
+	// let count = 0;
+	// const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+	// for (let i = 0; i < str.length; i++) {
+	// 	if (vowels.includes(str[i])) {
+	// 		count++;
+	// 	}
+	// }
+	// return count;
+
+	// Solution 2 -
+	return str.replace(/[^aeiou]/gi, '').length;
+}
 
 module.exports = countVowels;
