fc7fab184e4ca982bcbafce0b99e2ec35f246bab
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-11-20 09:55:30 -0500

    Finish 'titleCase'.

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,8 @@
-function titleCase() {}
+function titleCase(str) {
+	return str
+		.split(' ')
+		.map((word) => `${word[0].toUpperCase()}${word.slice(1).toLowerCase()}`)
+		.join(' ');
+}
 
 module.exports = titleCase;
