6b0ba7ba5f9a5d927e1a3b3a5421b8b2286deebe
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-11-20 12:11:26 -0500

    Finish 'reverseString' with 2 solutions.

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,14 @@
-function reverseString() {}
+function reverseString(str) {
+	// My solution 1 -
+	// let revStr = '';
+
+	// for (let i = str.length - 1; i >= 0; i--) {
+	// 	revStr += str[i];
+	// }
+	// return revStr;
+
+	// My solution 2 -
+	return str.split('').reverse().join('');
+}
 
 module.exports = reverseString;
