e6047755bbfe7d74b8c4f119c45c956cf2f07acb
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-11-20 16:19:41 -0500

    Finish 'removeDuplicates' with 3 solutions.

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,19 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+	// Solution 1 -
+	// const noDuplicates = [];
+
+	// for (let i = 0; i < arr.length; i++) {
+	// 	if (!noDuplicates.includes(arr[i])) {
+	// 		noDuplicates.push(arr[i]);
+	// 	}
+	// }
+	// return noDuplicates;
+
+	// Solution 2 -
+	// return arr.filter((el, i) => arr.indexOf(el) === i);
+
+	// Solution 3 -
+	return [...new Set(arr)];
+}
 
 module.exports = removeDuplicates;
