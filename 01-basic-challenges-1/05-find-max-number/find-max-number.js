892e7c4a472540de30de74a7425d11da29e5b76b
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-11-20 09:25:08 -0500

    Finish 'findMaxNumber'.

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,12 @@
-function findMaxNumber() {}
+function findMaxNumber(nums) {
+	let num = nums[0];
+
+	for (let i = 1; i < nums.length; i++) {
+		if (nums[i] > num) {
+			num = nums[i];
+		}
+	}
+	return num;
+}
 
 module.exports = findMaxNumber;
