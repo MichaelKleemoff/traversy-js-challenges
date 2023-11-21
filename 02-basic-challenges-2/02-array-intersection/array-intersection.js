043c3ee36f9c2417e03968a0e393dfe7563e2b5c
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-11-21 07:53:38 -0500

    Finish 'arrayIntersection' wit 2 solutions.

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,20 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+	// Solution 1 -
+	// const combinedArr = arr1.concat(arr2);
+	// const intersection = combinedArr.filter(
+	// 	(e, i) => combinedArr.indexOf(e) !== i
+	// );
+	// return intersection;
+
+	// Solution 2 -
+	const intersection = [];
+
+	for (let i = 0; i < arr1.length; i++) {
+		if (arr2.includes(arr1[i])) {
+			intersection.push(arr1[i]);
+		}
+	}
+	return intersection;
+}
 
 module.exports = arrayIntersection;
