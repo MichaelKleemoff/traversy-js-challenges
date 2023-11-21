77761425b6199ed2921a32b9ebf66fa67bf2b099
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-11-21 15:50:30 -0500

    Update for loop in 'arrayIntersection'. Delete prior 1st solution.

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,16 +1,9 @@
 function arrayIntersection(arr1, arr2) {
 	// Solution 1 -
-	// const combinedArr = arr1.concat(arr2);
-	// const intersection = combinedArr.filter(
-	// 	(e, i) => combinedArr.indexOf(e) !== i
-	// );
-	// return intersection;
-
-	// Solution 2 -
 	const intersection = [];
 
 	for (let i = 0; i < arr1.length; i++) {
-		if (arr2.includes(arr1[i])) {
+		if (arr2.includes(arr1[i]) && !intersection.includes(arr1[i])) {
 			intersection.push(arr1[i]);
 		}
 	}
