d68b09f8c80b87bffbc0f1883d13a1078db70c76
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-11-22 12:44:30 -0500

    Finish 'displayLikes'.

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,20 @@
-function displayLikes() {}
+function displayLikes(names) {
+	let message = '';
+	const numOthers = names.length;
+
+	if (numOthers === 0) {
+		message = `no one likes this`;
+	} else if (numOthers === 1) {
+		message = `${names[0]} likes this`;
+	} else if (numOthers === 2) {
+		message = `${names[0]} and ${names[1]} like this`;
+	} else if (numOthers === 3) {
+		message = `${names[0]}, ${names[1]} and ${names[2]} like this`;
+	} else {
+		message = `${names[0]}, ${names[1]} and ${numOthers - 2} others like this`;
+	}
+
+	return message;
+}
 
 module.exports = displayLikes;
