8c85d824e6f06079401a93995d807eb89860d0af
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-11-24 13:42:50 -0500

    Finish 'findMissingNumber' with 2 solutions.

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,21 @@
-function findMissingNumber() {}
+function findMissingNumber(nums) {
+	const n = nums.length + 1;
+
+	if (nums.length === 0) return 1;
+	if (!nums) return undefined;
+
+	const expectedSum = (n * (n + 1)) / 2;
+	// With `for loop` -
+	// let actualSum = 0;
+
+	// for (let i = 0; i < nums.length; i++) {
+	// 	actualSum += nums[i];
+	// }
+
+	// With `.reduce()` method -
+	let actualSum = nums.reduce((acc, cur) => acc + cur, 0);
+
+	return expectedSum - actualSum;
+}
 
 module.exports = findMissingNumber;
