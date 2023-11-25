1dedb55a9dc193c4b7a5feaa70297eb383508c89
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-11-25 10:45:44 -0500

    Finish 'findMissingLetter' with 2 solutions using 'indexOf', 'charCodeAt'.

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,32 @@
-function findMissingLetter() {}
+// Solution 1 - Using `.indexOf()`
+// function findMissingLetter(letters) {
+// 	const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// 	const startIndex = alphabet.indexOf(letters[0]);
+
+// 	for (let i = 0; i < letters.length; i++) {
+// 		if (letters[i] !== alphabet[startIndex + i]) {
+// 			return alphabet[startIndex + i];
+// 		}
+// 	}
+
+// 	return '';
+// }
+
+// Solution 2 - Using '.charCodeAt()`
+function findMissingLetter(letters) {
+	let start = letters[0].charCodeAt(0);
+
+	for (let i = 0; i < letters.length; i++) {
+		const current = letters[i].charCodeAt(0);
+
+		if (current - start > 1) {
+			return String.fromCharCode(start + 1);
+		}
+
+		start = current;
+	}
+
+	return '';
+}
 
 module.exports = findMissingLetter;
