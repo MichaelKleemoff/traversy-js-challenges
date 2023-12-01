3b5b787dc86b541eda1e84fbe67e5316cda55fb2
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-11-30 21:24:00 -0500

    Finish 'findFirstNonRepeatingCharacter' with 2 solutions.

--- 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
+++ 02-basic-challenges-2/07-first-non-repeating/first-non-repeating.js
@@ -1,3 +1,32 @@
-function findFirstNonRepeatingCharacter() {}
+function findFirstNonRepeatingCharacter(str) {
+	// Brad's Solution 1 - using object
+	// const charCount = {};
+
+	// for (const char of str) {
+	// 	charCount[char] = (charCount[char] || 0) + 1;
+	// }
+
+	// for (const char of str) {
+	// 	if (charCount[char] === 1) {
+	// 		return char;
+	// 	}
+	// }
+	// return null;
+
+	// Brad's Solution 2 - Using `Map()`
+	const charCount = new Map();
+
+	for (const char of str) {
+		charCount.set(char, (charCount.get(char) || 0) + 1);
+	}
+
+	for (const char of str) {
+		if (charCount.get(char) === 1) {
+			return char;
+		}
+	}
+
+	return null;
+}
 
 module.exports = findFirstNonRepeatingCharacter;
