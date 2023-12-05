4fefff0121c56871fdb7c1ac2df632e9df429460
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-12-05 09:15:15 -0500

    Finish 'validateEmail' with 2 solutions.

--- 02-basic-challenges-2/10-validate-email/validate-email.js
+++ 02-basic-challenges-2/10-validate-email/validate-email.js
@@ -1,3 +1,28 @@
-function validateEmail() {}
+function validateEmail(email) {
+	// Solution 1 - Using `regex`
+	// const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+	// return emailRegex.test(email);
+
+	// Solution 2 - Without `regex`
+	if (email.indexOf('@') === -1) {
+		return false;
+	}
+
+	// Destructuring an array
+	const [localPart, domain] = email.split('@');
+
+	if (localPart.length === 0 || domain.length < 3) {
+		return false;
+	}
+
+	const domainExtension = domain.split('.');
+
+	if (domainExtension.length < 2 || domainExtension[1] < 2) {
+		return false;
+	}
+
+	return true;
+}
 
 module.exports = validateEmail;
