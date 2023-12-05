a7a8139ba81ba7c3473f91cf75891e0520a12644
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-12-05 06:00:17 -0500

    Finish 'formatPhoneNumber' with 3 solutions.

--- 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
+++ 02-basic-challenges-2/09-format-phone-number/format-phone-number.js
@@ -1,3 +1,31 @@
-function formatPhoneNumber() {}
+function formatPhoneNumber(numbers) {
+	// Solution 1 -
+	// const areaCode = [];
+	// const prefix = [];
+	// const prefixUnique = [];
+
+	// for (let i = 0; i < numbers.length; i++) {
+	// 	if (i < 3) {
+	// 		areaCode.push(numbers[i]);
+	// 	} else if (i < 6) {
+	// 		prefix.push(numbers[i]);
+	// 	} else {
+	// 		prefixUnique.push(numbers[i]);
+	// 	}
+	// }
+	// return `(${areaCode.join('')}) ${prefix.join('')}-${prefixUnique.join('')}`;
+
+	// Solution 2 -
+	// const areaCode = numbers.slice(0, 3).join('');
+	// const prefix = numbers.slice(3, 6).join('');
+	// const prefixUnique = numbers.slice(6).join('');
+	// return `(${areaCode}) ${prefix}-${prefixUnique}`;
+
+	// Solution 3 -
+
+	return `(${numbers.slice(0, 3).join('')}) ${numbers
+		.slice(3, 6)
+		.join('')}-${numbers.slice(6).join('')}`;
+}
 
 module.exports = formatPhoneNumber;
