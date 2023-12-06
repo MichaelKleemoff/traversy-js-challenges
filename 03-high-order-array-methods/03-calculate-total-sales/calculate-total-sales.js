37b055dd92066e1299b920355857ddf4fa2a5d21
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-12-06 10:45:04 -0500

    Finish 'calculateTotalSalesWithTax'.

--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,10 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(products, taxRate) {
+	const subTotal = products
+		.map(({ price, quantity }) => price * quantity)
+		.reduce((sum, sale) => sum + sale, 0);
+	const tax = subTotal * taxRate * 0.01;
+
+	return subTotal + tax;
+}
 
 module.exports = calculateTotalSalesWithTax;
