3386e6c763a06c3c374d21c9aed1a3b1c623e89d
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-12-07 13:32:56 -0500

    Finish 'highestScoringWord' with 2 solutions.

--- 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
+++ 03-high-order-array-methods/04-highest-scoring-word/highest-scoring-word.js
@@ -1,3 +1,40 @@
-function highestScoringWord() {}
+function highestScoringWord(str) {
+	const words = str.split(' ');
+
+	const scores = words.map((word) => {
+		// 1st version -
+		// let score = 0;
+
+		// for (const letter of word) {
+		// 	score += letter.charCodeAt(0) - 96;
+		// }
+
+		// return score;
+
+		// 2nd version -
+		return Array.from(word).reduce(
+			(score, letter) => score + letter.charCodeAt(0) - 96,
+			0
+		);
+	});
+
+	// 1st version -
+	// let highestScore = scores[0];
+	// let highestIndex = 0;
+
+	// for (let i = 1; i < scores.length; i++) {
+	// 	if (scores[i] > highestScore) {
+	// 		highestScore = scores[i];
+	// 		highestIndex = i;
+	// 	}
+	// }
+
+	// 2nd version -
+	const highestScore = Math.max(...scores);
+	const highestIndex = scores.indexOf(highestScore);
+	console.log(highestScore);
+
+	return words[highestIndex];
+}
 
 module.exports = highestScoringWord;
