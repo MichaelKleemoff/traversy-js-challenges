9658c8dc4bc40e72beecf819fedb35fb4aee467d
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-12-13 06:21:30 -0500

    Finish 'generateHashtag'.

--- 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
+++ 03-high-order-array-methods/06-hashtag-generator/hashtag-generator.js
@@ -1,3 +1,26 @@
-function generateHashtag() {}
+function generateHashtag(str) {
+	// My Solution 1 - Using `.map()`
+	// let hashTagWord = '';
+
+	// if (str.trim() === '' || str === ' ' || str.length > 140) return false;
+
+	// hashTagWord = str
+	// 	.trim()
+	// 	.split(/\s+/)
+	// 	.map(
+	// 		(word) => `${word.charAt(0).toUpperCase()}${word.slice(1).toLowerCase()}`
+	// 	)
+	// 	.join('');
+
+	// return `#${hashTagWord}`;
+
+	// Brad's Solution 2 - Using `.reduce()`
+
+	const hashtag = str.split(' ').reduce(function (tag, word) {
+		return tag + word.charAt(0).toUpperCase() + word.substring(1).toLowerCase();
+	}, '#');
+
+	return hashtag.length === 1 || hashtag.length > 140 ? false : hashtag;
+}
 
 module.exports = generateHashtag;
