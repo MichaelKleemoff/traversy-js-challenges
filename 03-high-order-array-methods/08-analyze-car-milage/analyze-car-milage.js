c54af152c8b2618a30047967d82b1fdeba958f67
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-12-21 07:35:49 -0500

    Finish 'analyzeCarMileage'.

--- 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
+++ 03-high-order-array-methods/08-analyze-car-milage/analyze-car-milage.js
@@ -1,3 +1,56 @@
-function analyzeCarMileage() {}
+function analyzeCarMileage(cars) {
+	// Plan:
+	// 1. Create a new `cars` object
+	// Use an `object literal` and save it to a `variable`
+	const allCarsCalculations = {
+		totalMileage: 0,
+		highestMileageCar: 0,
+		lowestMileageCar: 0,
+		averageMileage: 0,
+	};
+
+	// 2. Iterate over the array of objects
+	// Use the `.reduce()`
+	// 3. Find the `total mileage` and save it to a new key in the new object
+	allCarsCalculations.totalMileage = Number(
+		cars.reduce((total, { mileage }) => total + mileage, 0).toFixed(2)
+	);
+
+	// 5. Find the `highest mileage` and save it to a new key in the new object
+	let highest = cars[0].mileage;
+
+	for (let i = 1; i < cars.length; i++) {
+		if (cars[i].mileage > highest) {
+			highest = cars[i].mileage;
+		}
+	}
+
+	allCarsCalculations.highestMileageCar = cars.find(
+		(car) => car.mileage === highest
+	);
+
+	// 6. Find the `lowest mileage` and save it to a new key in the new object
+	let lowest = cars[0].mileage;
+
+	for (let i = 1; i < cars.length; i++) {
+		if (cars[i].mileage < lowest) {
+			lowest = cars[i].mileage;
+		}
+	}
+
+	allCarsCalculations.lowestMileageCar = cars.find(
+		(car) => car.mileage === lowest
+	);
+
+	// 4. Get the `average` using `.reduce()` (getting total and dividing by `cars.length`) and save it to a new key in the new object
+	allCarsCalculations.averageMileage = Number(
+		(
+			cars.reduce((total, { mileage }) => total + mileage, 0) / cars.length
+		).toFixed(2)
+	);
+
+	// 7. Return the new object
+	return allCarsCalculations;
+}
 
 module.exports = analyzeCarMileage;
