7637785d3a5be0e7af1cd0907ca8623468bd7c3c
Michael Kleemoff <michaelkleemoff2@gmail.com>
2025-02-19 11:07:33 -0500

    Finish 'reverseString'.

--- 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
+++ 04-recursion/03-reverse-string-recursion/reverse-string-recursion.js
@@ -1,3 +1,11 @@
-function reverseString() {}
+function reverseString(str) {
+	// Base case
+	if (str === '') {
+		return '';
+	}
+
+	// Recursive case
+	return reverseString(str.substr(1)) + str.charAt(0);
+}
 
 module.exports = reverseString;
