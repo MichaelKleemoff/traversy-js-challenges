ae9ad53cb520aab1d6a9362a404348f5cfcdb34a
Michael Kleemoff <michaelkleemoff2@gmail.com>
2025-02-19 11:17:11 -0500

    Finish 'fibonacci'.

--- 04-recursion/04-fibonacci-sequence/fibonacci.js
+++ 04-recursion/04-fibonacci-sequence/fibonacci.js
@@ -1,3 +1,11 @@
-function fibonacci() {}
+function fibonacci(num) {
+	// Base case
+	if (num < 2) {
+		return num;
+	}
+
+	// Recursive case
+	return fibonacci(num - 1) + fibonacci(num - 2);
+}
 
 module.exports = fibonacci;
