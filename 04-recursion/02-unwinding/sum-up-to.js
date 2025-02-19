6877bd5136f3ec3bd1a4ad607b479c38dc6f0233
Michael Kleemoff <michaelkleemoff2@gmail.com>
2025-02-19 07:57:55 -0500

    Finish 'sumUpTo'.

--- 04-recursion/02-unwinding/sum-up-to.js
+++ 04-recursion/02-unwinding/sum-up-to.js
@@ -1,3 +1,11 @@
-function sumUpTo() {}
+function sumUpTo(num) {
+	// Base case
+	if (num === 1) {
+		return 1;
+	}
+
+	// Recursive case
+	return num + sumUpTo(num - 1);
+}
 
 module.exports = sumUpTo;
