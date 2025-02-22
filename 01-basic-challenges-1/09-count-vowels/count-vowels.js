9a2764a18ca4c14602eb43d0674d4424f9766c63
Michael Kleemoff <michaelkleemoff2@gmail.com>
2025-02-22 10:02:55 -0500

    Add another solution to 'countVowels'.

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -12,6 +12,25 @@ function countVowels(str) {
 
 	// Solution 2 -
 	return str.replace(/[^aeiou]/gi, '').length;
+
+	// Solution 3 (Brad's solution) -
+	// const formattedStr = str.toLowerCase();
+	// let count = 0;
+
+	// for (let i = 0; i < formattedStr.length; i++) {
+	//   const char = formattedStr[i];
+
+	//   if (
+	//     char === 'a' ||
+	//     char === 'e' ||
+	//     char === 'i' ||
+	//     char === 'o' ||
+	//     char === 'u'
+	//   ) {
+	//     count++;
+	//   }
+	// }
+	// return count;
 }
 
 module.exports = countVowels;
