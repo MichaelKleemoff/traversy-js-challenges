88df4f33276b213b1bde4f7b62792470540fc771
Michael Kleemoff <michaelkleemoff2@gmail.com>
2025-02-22 10:42:56 -0500

    Add some more results to 'display-likes-run.js'.

--- 02-basic-challenges-2/03-display-likes/display-likes-run.js
+++ 02-basic-challenges-2/03-display-likes/display-likes-run.js
@@ -1,5 +1,11 @@
 const displayLikes = require('./display-likes');
 
 const result = displayLikes(['Alex', 'Jacob', 'Mark', 'Max', 'Jill']);
+const result2 = displayLikes(['Alex']);
+const result3 = displayLikes(['Alex', 'Jacob']);
+const result4 = displayLikes([]);
 
 console.log(result);
+console.log(result2);
+console.log(result3);
+console.log(result4);
